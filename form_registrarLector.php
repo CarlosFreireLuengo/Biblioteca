47c9b077d7e5c6b2ff08884c02138fb2890830b7
Antonio Alonso <tonialonsoalonso15@gmail.com>
2025-10-28 11:54:29 +0100

    Archivos form subidos

---
+++ form_registrarLector.php
@@ -0,0 +1,18 @@
+<?php ?>
+<!--Formulario registro lectores-->
+<form action="" method="post">
+    <h2>Registrar lector</h2>
+
+    <label for="nombre">Nombre:</label>
+    <input type="text" id="nombre" name="nombre" required><br><br>
+
+    <label for="ape">Apellidos:</label>
+    <input type="text" id="ape" name="ape" required><br><br>
+
+    <label for="nombre">DNI:</label>
+    <input type="text" id="dni" name="dni" required><br><br>
+
+    <button type="submit" name="registrar">Registrar usuario</button>
+
+
+</form>
