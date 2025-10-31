759888efd728b456761e309f1608682eaf8decb8
Antonio Alonso <tonialonsoalonso15@gmail.com>
2025-10-31 12:22:50 +0100

    Función registrar lector

--- form_registrarLector.php
+++ form_registrarLector.php
@@ -1,18 +1,43 @@
-<?php ?>
+
+<?php
+ $conexion = new mysqli("localhost", "root", "", "biblioteca");
+if(isset($_POST["registrar"])){
+    $nombre = ($_POST["nombre"]);
+    $dni = ($_POST["dni"]);
+    $estado = "activo";
+    $n_prestados =0;
+
+    $compdni = "SELECT * FROM lectores WHERE DNI = '$dni'";
+    $resultado = $conexion->query($compdni);
+
+    if($resultado->num_rows>0){
+        $mensaje = "Ya existe un lector registrado con ese DNI";
+    }else{
+        $sql_addLector = "INSERT into lectores (lector_nombre, DNI, estado, n_prestados)
+                        VALUES ('$nombre', '$dni', '$estado','$n_prestados')";
+
+        if($conexion->query($sql_addLector)){
+            $mensaje="Lector registrado con éxito";
+        }else{
+            $mensaje="Error al resgistrar el lector: ". $conexion->error;
+        }
+    }
+}
+
+?>
 <!--Formulario registro lectores-->
 <form action="" method="post">
     <h2>Registrar lector</h2>
-
-    <label for="nombre">Nombre:</label>
+     <?php 
+    if (isset($mensaje)) {
+        echo "<p><strong>$mensaje</strong></p>";
+    }
+    ?>
+    <label for="nombre">Nombre y apellidos:</label>
     <input type="text" id="nombre" name="nombre" required><br><br>
-
-    <label for="ape">Apellidos:</label>
-    <input type="text" id="ape" name="ape" required><br><br>
-
-    <label for="nombre">DNI:</label>
+    <label for="dni">DNI:</label>
     <input type="text" id="dni" name="dni" required><br><br>
 
     <button type="submit" name="registrar">Registrar usuario</button>
-
 
 </form>
